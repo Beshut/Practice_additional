45d4159c1533d08f30d0d6d015781cb6b2ec4f4b
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:56:27 +0300

    Delete Program6.cs

--- Program6.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class Program
-{
-    static void Main(string[] args)
-    {
-        string s12 = "Hello";//обьявление и заполнение переменной типа string
-        Console.WriteLine("Get element by index s12[3]: " + s12[3]);//вывод на экран сообщение пользователю, обьединение строк
-    }
-}
